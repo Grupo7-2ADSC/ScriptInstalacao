e6cf08235b037729a0680daa56eeaf2f71752512
CaioAraruna <caio.araruna@sptech.school>
2024-06-10 20:32:03 -0300

    Novo Script de Instalação

--- Instalacao.sh
+++ Instalacao.sh
@@ -1,26 +1,36 @@
-#!/bin/bash
-
-# Função para exibir a barra de progresso
-progress_bar() { 
-    local duration=$1 
-    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done } 
-    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done } 
-    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); } 
-    clean_line() { printf "\r"; } 
-    for ((elapsed=1; elapsed<=$duration; elapsed++)) 
-    do 
-        already_done; remaining; percentage 
-        sleep 1 
-        clean_line 
-    done 
+# Função para barra de progresso
+progress_bar() {
+    local duration=$1
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    clean_line() { printf "\r"; }
+    for ((elapsed=1; elapsed<=$duration; elapsed++))
+    do
+        already_done; remaining; percentage
+        sleep 1
+        clean_line
+    done
     printf "\n"
 }
 
-echo "Olá! Seja bem-vindo(a) ao ambiente de instalação da Sentinel System. Vou te auxiliar no processo de instalação."
+clear
+
+# Função bot_message (assumindo que você tenha algo para lidar com isso)
+bot_message() {
+    echo "$1"
+}
+
+bot_success() {
+    echo "$1"
+}
+
+bot_message "Olá! Seja bem-vindo(a) ao ambiente de instalação da Sentinel System. Vou te auxiliar no processo de instalação."
 sleep 3
 
 echo "Podemos iniciar o processo de instalação? [s/n]"
 read confirmation
+
 if [[ "$confirmation" =~ ^[Ss]$ ]]; then
     echo "Iniciando instalação"
     sleep 3
@@ -38,7 +48,7 @@ if [[ "$confirmation" =~ ^[Ss]$ ]]; then
         read get_java
         if [[ "$get_java" =~ ^[Ss]$ ]]; then
             sudo apt install -y openjdk-17-jre
-            (progress_bar 10) & 
+            (progress_bar 10) &
             pid=$!
             wait $pid
             echo "Java instalado com sucesso!"
@@ -46,55 +56,150 @@ if [[ "$confirmation" =~ ^[Ss]$ ]]; then
         fi
     fi
 
-    echo "Agora precisamos fazer a instalação do Docker e a criação de containers. Essas ferramentas vão garantir que nosso sistema funcione adequadamente e com um melhor desempenho."
-    sleep 4
-    echo "Podemos iniciar? [s/n]"
-    read get
-    if [[ "$get" =~ ^[Ss]$ ]]; then 
-        sudo apt install -y docker.io
-        (progress_bar 10) & 
-        pid=$!
-        wait $pid
-                
-        echo "Docker já instalado, agora iremos iniciá-lo"
-        sudo systemctl start docker
-        sudo systemctl enable docker
+    # Verificar se o Docker está instalado
+    echo "Agora vamos verificar se você possui o Docker instalado."
+    if ! [ -x "$(command -v docker)" ]; then
+        echo "Notamos que você não possui o Docker instalado, vamos dar início à instalação!"
         sleep 3
-        (progress_bar 10) & 
-        pid=$!
-        wait $pid
-
-        echo "Docker iniciado!"
-        sleep 4
-
-        echo "Agora que já temos nosso ambiente Docker, vamos criar um contêiner para rodar o Git."
-        sleep 3
-
-        sudo docker run -it --name git-container ubuntu:latest bash -c "apt update && apt install -y git && git clone https://github.com/Grupo7-2ADSC/Jar-Gp7.git"
-        (progress_bar 5) & 
-        pid=$!
-        wait $pid
-
-        echo "Clonagem do repositório concluída com sucesso"
-        sleep 3
-
-        echo "Nosso próximo passo é instalar um banco de dados. Isso permite que possamos armazenar os dados capturados, criar gráficos dinâmicos e gerar relatórios"
-        sleep 4
-
-        sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=sentinel_system" -e "MYSQL_ROOT_PASSWORD=@Thigas844246" mysql:5.7
-        echo "Esperando o contêiner inicializar..."
-        sleep 30 # Ajuste conforme necessário para garantir que o MySQL esteja pronto
-
-        echo "Excelente!! Os containers e Docker já foram criados e a conexão com o banco está pronta."
-        sleep 4
-        echo "Instalação finalizada! Pressione Enter para sair."
-        read
-    else
-        echo "Infelizmente você não quis prosseguir e isso pode gerar falhas na execução do sistema. Caso mude de ideia, estamos prontos para continuar a instalação."
-        sleep 4
+        progress_bar 3
+        sudo apt-get update
+        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update
+        sudo apt-get install -y docker-ce
     fi
-else
-    echo "Instalação abortada pelo usuário."
+
+    # Verificar se o Docker Compose está instalado
+    if ! [ -x "$(command -v docker-compose)" ]; then
+        echo "Você também não possui o Docker Compose em sua máquina, vamos dar sequência na instalação."
+        sleep 3
+        progress_bar 3
+        sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        clear
+    fi
+
+    # Verificar se o Maven está instalado
+    if ! [ -x "$(command -v mvn)" ]; then
+        echo "Maven não está instalado. Instalando Maven..."
+        sleep 3
+        progress_bar 3
+        sudo apt-get install -y maven
+        clear
+    fi
+
+    # Criar diretório para yml
+    mkdir -p dockerCompose
+
+    # Ir para o diretório
+    cd dockerCompose
+
+    # Clonar o repositório GitHub
+    echo "Vamos clonar o repositório para funcionamento do sistema"
+    sleep 3
+    progress_bar 5
+    git clone https://github.com/Grupo7-2ADSC/Jar-Gp7.git
+    clear
+
+    # Construir o projeto usando Maven
+    echo "Construindo o projeto Maven"
+    sleep 3
+    progress_bar 5
+    cd Jar-Gp7
+    mvn clean install
+    clear
+
+    # Verificar se o JAR foi criado corretamente
+    if [ ! -f "SSS.jar" ]; then
+        echo "Erro: Não foi possível localizar o arquivo JAR após a construção do Maven."
+        exit 1
+    fi
+
+    # Voltar para o diretório dockerCompose
+    cd ..
+
+    # Criar o arquivo docker-compose.yml
+    bot_message "Criando o arquivo docker-compose.yml..."
+    progress_bar 3
+    cat <<EOL > docker-compose.yml
+version: '3.3'
+services:
+  mysql:
+    container_name: containerBD
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: urubu100
+      MYSQL_DATABASE: root
+    volumes:
+      - ./init-scripts:/docker-entrypoint-initdb.d
+    ports:
+      - "3306:3306"
+  java:
+    image: openjdk:17
+    container_name: javaSentinelSystem
+    depends_on:
+      - mysql
+    volumes:
+      - ./Jar-Gp7:/JAR Grupo
+    working_dir: /JAR Grupo
+    command: ["java", "-jar", "SSS.jar"]
+EOL
+
+    # Criar diretório init-scripts
+    echo "Agora estamos dando sequência ao procedimento para perfeita execução do sistema"
     sleep 2
+    progress_bar 3
+    mkdir -p init-scripts
+
+    # Iniciar Docker
+    echo "Iniciando o Docker ..."
+    sleep 2
+    sudo systemctl start docker
+
+    # Iniciar Docker Compose
+    echo "Iniciando o Docker Compose..."
+    sleep 2
+    progress_bar 5
+    sudo docker-compose up -d
+
+    # Aguardar os containers iniciarem
+    echo "Aguardando os containers iniciarem..."
+    sleep 2
+    progress_bar 5
+    sleep 5
+
+    # Verificar se os containers estão em execução
+    if [ "$(sudo docker inspect -f '{{.State.Running}}' javaSentinelSystem)" == "true" ]; then
+        echo "Todos os containers estão em execução."
+        sleep 2
+    else
+        echo "O container Java não está em execução. Verifique os logs para mais detalhes."
+        sleep 2
+        sudo docker-compose logs
+        exit 1
+    fi
+
+    # Perguntar ao usuário se ele deseja executar o JAR
+    echo "Deseja executar o arquivo JAR agora (S/N)?"
+    read exec_jar
+
+    if [[ "$exec_jar" =~ ^[Ss]$ ]]; then
+        echo "Executando o arquivo JAR..."
+        sleep 2
+        sudo docker-compose exec java java -jar SSS.jar
+    else
+        echo "Você optou por não executar o JAR agora. Até a próxima!"
+        sleep 2
+    fi
+
+    # Adicionar alias ao arquivo .bashrc
+    echo "Adicionando apelido para facilitar a execução do JAR."
+    sleep 2
+    echo "alias executar='sudo docker-compose exec java java -jar SSS.jar'" >> ~/.bashrc
+    source ~/.bashrc
+
+    bot_success "Instalação concluída!"
 fi
 
